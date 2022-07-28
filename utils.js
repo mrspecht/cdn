b64c08546be8a1086bbcac5f01373b5de77a4072
Andre Specht <andrespecht@outlook.com>
2022-07-28 10:08:14 -0500

    Add utils

---
+++ utils.js
@@ -0,0 +1,53 @@
+'use strict';
+
+// Add event listener
+function onEvent(event, selector, callback) {
+  return selector.addEventListener(event, callback);
+}
+
+// Get HTML element by id
+function getElement(selector, parent = document) {
+  return parent.getElementById(selector);
+}
+
+// Select HTML element
+function select(selector, parent = document) {
+  return parent.querySelector(selector);
+}
+
+// Select all HTML elements
+function selectAll(selector, parent = document) {
+  return parent.querySelectorAll(selector);
+}
+
+// Get a (node) list of HTML elements
+function selectAll(selector, parent = document) {
+  return [...parent.querySelectorAll(selector)];
+}
+
+// Print
+function print(arg) {
+  console.log(arg);
+}
+
+// Sleep
+function sleep(duration) {
+  return new Promise(resolve => {
+    setTimeout(resolve, duration)
+  });
+}
+
+// Generate random number between - and including - 'min' and 'max'
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// Filter array
+function filterArray(array, callback) {
+  return array.filter(callback);
+}
+
+// Create an HTML element
+function create(element, parent = document) {
+  return parent.createElement(element);
+}
