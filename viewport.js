863d452446b6e31f01a1f8bd9b5defc07ec1df8c
Andre Specht <andrespecht@outlook.com>
2022-11-29 12:02:37 -0600

    Update viewport.js

--- viewport.js
+++ viewport.js
@@ -1,8 +1,10 @@
+'use strict';
+
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 // Listening to the resize event
 window.addEventListener('resize', () => {
-  let vh = window.innerHeight * 0.01;
+  vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
